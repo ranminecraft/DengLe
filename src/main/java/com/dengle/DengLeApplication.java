6872a435066a27bce20ff15bf3ab4a4cab92a921
Ran <xyfwdy@gmail.com>
2024-06-26 19:16:48 +0800

    20240626

---
+++ src/main/java/com/dengle/DengLeApplication.java
@@ -0,0 +1,13 @@
+package com.dengle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DengLeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DengLeApplication.class, args);
+    }
+
+}
